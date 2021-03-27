582dd55f3cd4825f510f3e2a81cb6d4a2e746223
jayakumari1503 <kumarijaya1503@gmail.com>
2021-03-28 00:12:34 +0530

    Java program to multiply two floating point numbers.

---
+++ Task0/Introduction/Task0_3.java
@@ -0,0 +1,12 @@
+//Java program to multiply two floating point numbers.
+package Task0.Introduction;
+
+public class Task0_3 {
+    public static void main(String[] args) {
+        float num1 = 15.6F;
+        float num2 = 18.9F;
+        float multiply = num1 * num2;
+        System.out.println( " Product : " + num1 + "  * " + num2 + " = " + multiply);
+    
+    }
+}
