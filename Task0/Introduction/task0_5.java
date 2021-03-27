b793fe530fde6dbffc1ea13a55feb01922daef51
jayakumari1503 <kumarijaya1503@gmail.com>
2021-03-28 00:24:31 +0530

    //Java program to compute quotient and remainder.

---
+++ Task0/Introduction/task0_5.java
@@ -0,0 +1,13 @@
+//Java program to compute quotient and remainder.
+package Task0.Introduction;
+
+public class task0_5 {
+    public static void main(string args[]) {
+        int dividend = 67;
+        int divisor = 5;
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+        System.out.println(" Quotient = " + quotient);
+        System.out.println(" Remainder = " + remainder);   
+    }
+}
