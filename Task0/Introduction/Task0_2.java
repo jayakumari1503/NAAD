7bb79832288a9527489884932231da7681b942e4
jayakumari1503 <kumarijaya1503@gmail.com>
2021-03-28 00:11:17 +0530

    Java program to add two integers .

---
+++ Task0/Introduction/Task0_2.java
@@ -0,0 +1,11 @@
+//Java program to add two integers .
+package Task0.Introduction;
+public class Task0_2 {
+    public static void main(String[] args) {
+
+        int num1 = 78;
+        int num2 = 56;
+        int sum = num1 + num2;
+        System.out.println(" sum : " + num1 + " + " + num2 + " = " + sum);
+    }
+    }
