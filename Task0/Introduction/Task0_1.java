61b9c96c171f1185613cabf6fec0a5b8aea28302
jayakumari1503 <kumarijaya1503@gmail.com>
2021-03-28 00:09:41 +0530

    java program to print an integer (entered by the user)

---
+++ Task0/Introduction/Task0_1.java
@@ -0,0 +1,15 @@
+//java program to print an integer (entered by the user)
+package Task0.Introduction;
+import java.util.Scanner;
+
+public class Task0_1 {
+
+         public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Enter an integer number");
+        int N = scanner.nextInt();
+        System.out.println(" you entered : " + N);
+            
+        }
+}
