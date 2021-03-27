e4121cd2bbef0f8ae75a299beb7c69aff6632310
jayakumari1503 <kumarijaya1503@gmail.com>
2021-03-28 00:51:30 +0530

    Java program to swap two numbers.

---
+++ Task0/Introduction/Task0_6.java
@@ -0,0 +1,20 @@
+//Java program to swap two numbers.
+package Task0.Introduction;
+
+import java.util.*;
+ class Task0_6 {
+    public static void main(String args[]) {
+        int x;
+        int y;
+        int z;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Enter the value of x and y :");
+        x = scanner.nextInt();
+        y = scanner.nextInt();
+        System.out.println(" Before swapping :" + " x = " + x + " and "+ " y =" + y);
+        z = x;
+        x = y;
+        y =z;
+        System.out.println(" After swapping " + " x = " + x + " and " + " y = " + y);
+    }
+}
