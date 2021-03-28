d9d28c1d836e45f1bcda55c18f03bcff0629c07a
jayakumari1503 <kumarijaya1503@gmail.com>
2021-03-28 10:39:24 +0530

    Java program to find the frequency of character in a string.

---
+++ Task0/Introduction/Task0_11.java
@@ -0,0 +1,19 @@
+//Java program to find the frequency of character in a string.
+package Task0.Introduction;
+
+public class Task0_11 {
+    public static void main(String args[]) {
+        String str = " Hello everyone.";
+        char ch = 'e';
+        int frequency = 0;
+
+        for(int i = 0; i<str.length(); i++) {
+            if(ch == str.charAt(i)) {
+            }
+        } 
+        
+        System.out.println(" Frequency of " + ch + " = " + frequency);
+      
+    }
+    
+}
