173dfcc6e78e8020f4a8be4184ef794794de4bce
jayakumari1503 <kumarijaya1503@gmail.com>
2021-03-28 11:26:11 +0530

    Java program to check if a string is empty or null.

---
+++ Task0/Introduction/Task0_14.java
@@ -0,0 +1,18 @@
+//Java program to check if a string is empty or null.
+package Task0.Introduction;
+
+import java.util.Scanner;
+
+public class Task0_14 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        if(str == null || str.isEmpty() )
+        System.out.println(" String is empty or null");
+        
+        else
+        System.out.println(" String is not empty or null"); 
+    }
+    
+}
