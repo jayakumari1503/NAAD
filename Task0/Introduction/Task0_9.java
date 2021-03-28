8ea7ba29311405d1f57ae7f5adbc144a83bf37f5
jayakumari1503 <kumarijaya1503@gmail.com>
2021-03-28 09:39:25 +0530

    Java program to find the largest among three number.

---
+++ Task0/Introduction/Task0_9.java
@@ -0,0 +1,33 @@
+//Java program to find the largest among three number.
+package Task0.Introduction;
+
+import java.util.Scanner;
+
+public class Task0_9 {
+ public static void main(String args[]) {
+     int x;
+     int y;
+     int z;
+
+    Scanner scanner = new Scanner(System.in);
+
+     System.out.println(" Enter the first number : ");
+     x = scanner.nextInt();
+
+     System.out.println(" Enter the second number : ");
+     y = scanner.nextInt();
+
+     System.out.println(" Enter the third number : ");
+     z = scanner.nextInt();
+
+     if(x > y && x > z)
+            System.out.println(" Largest number = " + x);
+    else if(y > z)
+            System.out.println(" Largest number = " + y);
+
+    else
+            System.out.println(" Largest number = " + z);
+
+    }
+
+}
