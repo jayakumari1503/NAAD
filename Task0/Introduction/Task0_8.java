08b52bc438b787f369cd780dfce370223bcdcdf4
jayakumari1503 <kumarijaya1503@gmail.com>
2021-03-29 10:53:57 +0530

    Java program to check whether a number is vowel or consonant.

--- Task0/Introduction/Task0_8.java
+++ Task0/Introduction/Task0_8.java
@@ -7,6 +7,7 @@ public class Task0_8 {
   public static void main(String args[]) {
 
          Scanner scanner = new Scanner(System.in);
+         System.out.print(" Enter a character : ");
 
          char character = scanner.next().charAt(0);
 
