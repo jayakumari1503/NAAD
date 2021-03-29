d8735544a0e26272adea7eba9ce7684e3a06f488
jayakumari1503 <kumarijaya1503@gmail.com>
2021-03-29 13:41:26 +0530

    Revert "Java program to display fibonacci series."
    This reverts commit 1c32451b8284aeadddfa7dea038e48c86a2b0407.

--- Task0/TypeConversion/J02.java
+++ Task0/TypeConversion/J02.java
@@ -7,10 +7,10 @@ public class J02{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print(" Enter a number(int variable) : ");
+        System.out.println(" Enter a number(int variable) : ");
         int number  = scanner.nextInt();
         String str = String.valueOf(number);
-        System.out.print("Number in the form of string : " + str + ".");
+        System.out.println("Number in the form of string : " + str + " . ");
         
     }
 }
