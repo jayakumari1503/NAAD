1c32451b8284aeadddfa7dea038e48c86a2b0407
jayakumari1503 <kumarijaya1503@gmail.com>
2021-03-29 13:38:43 +0530

    Java program to display fibonacci series.

--- Task0/TypeConversion/J02.java
+++ Task0/TypeConversion/J02.java
@@ -7,10 +7,10 @@ public class J02{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println(" Enter a number(int variable) : ");
+        System.out.print(" Enter a number(int variable) : ");
         int number  = scanner.nextInt();
         String str = String.valueOf(number);
-        System.out.println("Number in the form of string : " + str + " . ");
+        System.out.print("Number in the form of string : " + str + ".");
         
     }
 }
